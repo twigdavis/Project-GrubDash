293111a5e83f83f539b0128c7816ebbcc90e12cd
Justin Davis <judavis@tesla.com>
2024-05-10 08:58:33 -0400

    C:/Users/judavis/AppData/Local/Programs/Git/Dishes GET and POST

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,66 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res) {
+    res.json({ data: dishes });
+}
+
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body;
+        if(data[propertyName]) {
+            return next();
+        }
+        next({ status: 400, message: `Must include a ${propertyName}`});
+    };
+}
+
+function propertyIsValidString(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body;
+        if (typeof data[propertyName] === 'string' && data[propertyName] != "" ) {
+            return next();
+        }
+        next({ status: 400, message: `${propertyName} must be a string!`});
+    };
+}
+
+function propertyIsValidInteger(propertyName) {
+    return function (req, res, next) {
+        const { data = {} } = req.body;
+        if (data[propertyName] <= 0 || !Number.isInteger(data[propertyName])) {
+            return next({ status: 400, message: `${propertyName} requires a valid number!`});
+        }
+        next();
+    };
+}
+
+function create(req, res) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    const newDish = {
+        id: nextId,
+        name,
+        description,
+        price,
+        image_url
+    };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
+
+
+module.exports = {
+    list,
+    create: [
+        bodyDataHas("name"),
+        propertyIsValidString("name"),
+        bodyDataHas("description"),
+        propertyIsValidString("description"),
+        bodyDataHas("price"),
+        propertyIsValidInteger("price"),
+        bodyDataHas("image_url"),
+        propertyIsValidString("image_url"),
+        create
+    ]
+}
